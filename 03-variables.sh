66f0794ba262fd99211c1fb25dcc20d8e2d99470
rganjare <rahul.ganjare@gmail.com>
2022-04-22 01:33:46 +0530

    updated variable.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,10 +7,20 @@ echo $b
 DATE=$(date +%F)
 echo "Today's" date is $DATE
 
-echo "Today's" date is $(date +%F)
+echo  Time is $(date +%T)
 
 x=10
 y=20
 z=$(($x+$y))
 
 echo Addition of $x and $y is $z
+
+## Array 
+
+c=(10 20 first second 45 third)
+
+echo ${c[0]}
+echo ${c[1]}
+echo ${c[2]}
+echo ${c[3]}
+echo ${c[*]}
