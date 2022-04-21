533551074bd56d1450009e692b6fbd39cfcecb2f
rganjare <rahul.ganjare@gmail.com>
2022-04-22 02:16:36 +0530

    updated file

--- 04-user_input.sh
+++ 04-user_input.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-read -p "Enter your Name: " NAME
+read -p "==> Enter your Name: " NAME
+echo ==> my name is $NAME
+echo "==> my name is $NAME"
 
-echo my name is $NAME
+
+##Special Variables
+## $0-$n, $*, $@, $#.
+#  $0 - Script Name
+#  $1 - $n - Arguments parsed in the order.
+#  $* , $@ - All arguments 
+#  $# - Number of arguments
+
+echo script Name         $0
+echo first argument      $1
+echo second argument     $2
+echo all arguments       $@
+echo all arguments       $*
+echo number of Arguments $#
+
