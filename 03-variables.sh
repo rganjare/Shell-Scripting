36e5677f4861ba3fa7486f1994e69896d826a285
rganjare <rahul.ganjare@gmail.com>
2022-04-22 00:52:19 +0530

    updated variable.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,8 +4,13 @@ b=Devops
 echo ${a}times
 echo $b
 
-
 DATE=$(date +%F)
 echo "Today's" date is $DATE
 
 echo "Today's" date is $(date +%F)
+
+x=10
+y=20
+z=$(($x+$y))
+
+echo Addition of $x and $y is $z
