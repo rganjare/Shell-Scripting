cfc620a13830c8a669d0480b720f721cb718cc1d
rganjare <rahul.ganjare@gmail.com>
2022-04-23 01:34:40 +0530

    added conditions.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,9 +1,7 @@
 
 echo "==> Welcome to FrontEnd Component !!!"
 
-USER_ID=$(id -u)
-
-echo "$USER_ID"
+USER_ID=$(sudo id -u)
 
 if [ $USER_ID -ne 0 ]; then
     echo -e "\e[33m You should run script with root user \e[0m"
@@ -12,9 +10,19 @@ fi
 
 echo -e "\e[33m Installing Nginx \e[0m"
 yum install nginx -y
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m==>SUCCESS\e[0m"
+else
+   echo -e "\e[31m==>Failuer\e[0m" 
+fi
 
 echo -e "\e[33m download the HTDOCS content and deploy under the Nginx path \e[0m"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m==>SUCCESS\e[0m"
+else
+   echo -e "\e[31m==>Failuer\e[0m" 
+fi
 
 echo -e "\e[33m Deploy in Nginx Default Location \e[0m"
 cd /usr/share/nginx/html
@@ -24,7 +32,17 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m==>SUCCESS\e[0m"
+else
+   echo -e "\e[31m==>Failuer\e[0m" 
+fi
 
 echo -e "\e[33m Finally restart the service once to effect the changes \e[0m"
 systemctl restart nginx
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m==>SUCCESS\e[0m"
+else
+   echo -e "\e[31m==>Failuer\e[0m" 
+fi
 systemctl enable nginx
