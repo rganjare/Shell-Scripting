115e8d3cdf34b59f2e0b7f47e92d1403c39cab00
rganjare <rahul.ganjare@gmail.com>
2022-04-23 00:35:31 +0530

    added conditions.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,6 +3,8 @@ echo "==> Welcome to FrontEnd Component !!!"
 
 USER_ID=$(id -u)
 
+echo "$USER_ID"
+
 if ["$USER_ID" -ne 0]; then
     echo -e "\e[33m You should run script with root user \e[0m"
     exit 1
@@ -11,10 +13,10 @@ fi
 echo -e "\e[33m Installing Nginx \e[0m"
 yum install nginx -y
 
-echo -e "\e[33 download the HTDOCS content and deploy under the Nginx path \e[0m"
+echo -e "\e[33m download the HTDOCS content and deploy under the Nginx path \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-echo -e "\e[33 Deploy in Nginx Default Location \e[0m"
+echo -e "\e[33m Deploy in Nginx Default Location \e[0m"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
@@ -23,6 +25,6 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "/e[33m Finally restart the service once to effect the changes /e[0m"
+echo -e "\e[33m Finally restart the service once to effect the changes \e[0m"
 systemctl restart nginx
 systemctl enable nginx
