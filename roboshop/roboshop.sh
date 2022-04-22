091c37d68e4afac06d689fed6858bdb8ad088a19
rganjare <rahul.ganjare@gmail.com>
2022-04-22 23:37:33 +0530

    added conditions.sh

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [!-e components/$1.sh ];then
-echo components/$1.sh does not exist 
-exit 
-else
-bash components/$1.sh
+if [-e components/$1.sh ]; then
+    bash components/$1.sh 
+else [
+    echo components/$1.sh does not exist 
+    exit
+    ]
 fi
