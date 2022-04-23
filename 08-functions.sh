584bf5172a73bef3353cd7c1b1d9cdb575b22057
rganjare <rahul.ganjare@gmail.com>
2022-04-24 05:10:31 +0530

    added function.sh

--- 08-functions.sh
+++ 08-functions.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Variable : If we assign a name to set of data that is variable
+# Function : If we assign a name to set of commands that is function
+
+# func_name() {
+# commands
+# commands
+# }
+
+# func_name
+
+## Declare a function
+
 PrintMessage(){
 echo My name is Rahul.
 echo I am learning $1.
@@ -9,12 +21,17 @@ b=20
 }
 
 a=20
-echo -e "\e[33m ===> I am running PrintMessage function \e[0m"
+
+echo -e "\e[33m ===> I am running PrintMessage function. \e[0m"
 PrintMessage Selenium
 
-echo -e "\e[33m ===> I am running main program \e[0m"
+
+
+echo -e "\e[33m ===> I am running main program. \e[0m"
 echo I want to get $1 lacs of package
 echo I want salary revision upto $b lacs in same company
+b=30
+echo The OverWritten revised salary upto $b lacs in same company
 
 STAT(){
     echo Hello
@@ -22,4 +39,9 @@ STAT(){
 }
 
 STAT
-echo Exit status of function STAT is $?
+echo -e "\e[32m ===> Exit status of function STAT is \e[0m" $?
+
+## Note, Function needs to declared first and then you call the function later in the code
+## Function will have it's own set of special variables
+## Variable declared in main program can be overwritten in function and vice versa.
+# Function is a command , Function have exit status well
