d48df4f6d8ab1ce08b1f1dd50815707f90b7da41
rganjare <rahul.ganjare@gmail.com>
2022-04-24 04:42:36 +0530

    added function.sh

--- 08-functions.sh
+++ 08-functions.sh
@@ -5,11 +5,11 @@ echo My name is Rahul.
 echo I am learning $1.
 echo I am very keen about it.
 echo I want to get $a lacs of package
-b=25
+b=20
 }
 
 a=20
 PrintMessage DevOps
 
 echo I want to get $1 lacs of package
-echo I want to get $b lacs of package
+echo I want salary revision upto $b lacs in same company
