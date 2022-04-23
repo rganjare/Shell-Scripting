3498b98dfaa5d0d171ab8d4e1e807ca031d9cef7
rganjare <rahul.ganjare@gmail.com>
2022-04-24 05:02:13 +0530

    added function.sh

--- 08-functions.sh
+++ 08-functions.sh
@@ -9,9 +9,17 @@ b=20
 }
 
 a=20
-echo I am running PrintMessage function
+echo -e "\e[33m ===> I am running PrintMessage function \e[0m"
 PrintMessage Selenium
 
-echo I am running main program
+echo -e "\e[33m ===> I am running main program \e[0m"
 echo I want to get $1 lacs of package
 echo I want salary revision upto $b lacs in same company
+
+STAT(){
+    echo Hello
+    return 1
+}
+
+STAT
+echo Exit status of function STAT is $?
