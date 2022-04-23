4e20d600442aebf1daabc384151e0b9ef8b2bc0b
rganjare <rahul.ganjare@gmail.com>
2022-04-24 04:39:33 +0530

    added function.sh

--- 08-functions.sh
+++ 08-functions.sh
@@ -4,7 +4,12 @@ PrintMessage(){
 echo My name is Rahul.
 echo I am learning $1.
 echo I am very keen about it.
+echo I want to get $a lacs of package
+b=25
 }
 
+a=20
+PrintMessage DevOps
 
-PrintMessage Selenium
+echo I want to get $1 lacs of package
+echo I want to get $b lacs of package
