86ad170db704bee47b7d2b1bd8c0cbc3797b7dd6
rganjare <rahul.ganjare@gmail.com>
2022-04-24 07:11:58 +0530

    Redirectors & Log_File

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,7 +18,8 @@ fi
 }
 
 Print(){
-echo -e "\e[33m ==> $1 \e[0m"
+    echo -e "\e[33m************************* $1 *********************************\e[0m" >> $LOG_FILE
+    echo -e "\e[33m ==> $1 \e[0m"
 }
 
 LOG_FILE=/tmp/roboshop.log
@@ -38,19 +39,19 @@ rm -rf *
 StatusCheck $? "Clean up old Nginx content"
 
 Print "Extracting Archive"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . 
+unzip /tmp/frontend.zip >> $LOG_FILE && mv frontend-main/* . >> $LOG_FILE && mv static/* . >> $LOG_FILE
 StatusCheck $? "Extracting Archive"
 
 Print "Updating RoboShop configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >> $LOG_FILE
 StatusCheck $? "Updating RoboShop configuration" 
 
 Print "Starting Nginx"
-systemctl restart nginx && systemctl enable nginx
+systemctl restart nginx && systemctl enable nginx >> $LOG_FILE
 StatusCheck $? "Starting Nginx" 
 
 Print "Frontend Application status check"
-systemctl status nginx -l
+systemctl status nginx -l >> $LOG_FILE
 if [ $? -eq 0 ]; then
 echo -e "\e[32m Cool!!! Frontend is up and Running !!! \e[0m"
 fi
