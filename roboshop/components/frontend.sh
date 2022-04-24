d2f7cf2e6a070f90b187d142f7dddce6ff5c3894
rganjare <rahul.ganjare@gmail.com>
2022-04-24 05:52:29 +0530

    added function.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,23 +9,21 @@ if [ $USER_ID -ne 0 ]; then
 fi
 
 StatusCheck(){
-if [ $2 -eq 0 ]; then
-    echo $1
-    echo -e "\e[32m ==> SUCCESS\e[0m"
+if [ $1 -eq 0 ]; then
+    echo -e "\e[32m ==> $2 ==> SUCCESS\e[0m"
 else
-    echo $1
-    echo -e "\e[31m ==> Failuer !!! \e[0m" 
+    echo -e "\e[31m ==> $2 ==> Failuer !!! \e[0m" 
     exit 2
 fi
 }
 
 echo -e "\e[33m Installing Nginx \e[0m"
 sudo yum install nginx -y
-StatusCheck -e "\e[33m ==> Nginx installation status \e[0m" $?
+StatusCheck $? "Nginx installation status" 
 
 echo -e "\e[33m Download the HTDOCS content and deploy under the Nginx path \e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
-StatusCheck -e "\e[33m ==> Download the HTDOCS content \e[0m" $?
+StatusCheck $? "Download the HTDOCS content status" 
 
 echo -e "\e[33m Deploy in Nginx Default Location \e[0m"
 cd /usr/share/nginx/html
