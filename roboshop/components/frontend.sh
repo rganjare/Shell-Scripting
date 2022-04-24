b495db1ca637c5e7b9b8564cf4dccccbcc7646a5
rganjare <rahul.ganjare@gmail.com>
2022-04-24 06:37:17 +0530

    Logical Oprtators

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -32,21 +32,22 @@ StatusCheck $? "Download the HTDOCS content"
 Print "Clean up old Nginx content"
 cd /usr/share/nginx/html
 rm -rf *
+StatusCheck $? "Clean up old Nginx content"
 
 Print "Extracting Archive"
 unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . 
-StatusCheck $?
+StatusCheck $? "Extracting Archive"
 
+Print "Updating RoboShop configuration"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-StatusCheck $? "Deploy in Nginx Default Location status" 
+StatusCheck $? "Updating RoboShop configuration" 
 
-Print "Finally restart the service to effect the changes"
-systemctl restart nginx
-StatusCheck $? "Restart service status" 
+Print "Starting Nginx"
+systemctl restart nginx && systemctl enable nginx
+StatusCheck $? "Starting Nginx" 
 
-Print "Enable Nginx service"
-systemctl enable nginx
-StatusCheck $? "Enable Nginx service status" 
-
+Print "Frontend Application status check"
 systemctl status nginx -l
-echo -e "\e[31m Cool!!! Frontend is up and Running !!! \e[0m"
+if [ $? -eq 0 ]; then
+echo -e "\e[32m Cool!!! Frontend is up and Running !!! \e[0m"
+fi
